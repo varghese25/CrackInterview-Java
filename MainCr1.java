820b26ad1ee409449dadaa54cd91e52ac8da3d74
Varghese Baby <Vbpoulose@gmail.com>
2025-09-30 18:13:57 -0400

    Java-MainClass - Main

---
+++ MainCr1.java
@@ -0,0 +1,15 @@
+public class MainCr1 {
+
+ static Float num;
+ int value = num.intValue();
+
+ public static void main(String[] args) {
+ System.out.println("Hii");
+ }
+}
+/**
+ * A. Hii
+B. NullPointerException at runtime
+C. Compilation error: variable num might not have been initialized
+D. No output
+ */
